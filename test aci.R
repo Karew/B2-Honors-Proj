71b0ccb0facfcadd62c1f527d18bcc67e8aedd78
Karew <karenwang@email.arizona.edu>
2015-10-08 10:50:10 -0700

    cleared up the comments

--- test aci.R
+++ test aci.R
@@ -1,22 +1,33 @@
-#read in data
-aci_test <- read.table(
+# Karen Wang
+# B2 Honors Project
+
+#This reads in data
+aci_test = read.table(
   "data//b2 pop a18 09-18-15",skip = 18,
   sep="\t", header=FALSE)
-#read in headers
-aci_test_header <- read.table(
+
+#This reads in headers
+aci_test_header = read.table(
   "data//b2 pop a18 09-18-15",skip = 16, nrows = 1,stringsAsFactors = FALSE,
   sep="\t", header=FALSE)
-#list
+
+#This lists our column names
 aci_columns= as.list(aci_test_header)
 colnames(aci_test)=aci_columns
 
-#plot
-plot(aci_test$Ci,aci_test$Photo)
-#making a list of all the files i want to read in
+#This plots Ci on the x and A on the y
+plot(aci_test$Ci,aci_test$Photo,main = "A Ci Curve", ylab = "Photosynthesis", xlab = "Ci")
+
+#making a list of all the files I want to read in from data folder
 aci_file_list=list.files(path="./data/",pattern=NULL)
 
-
+#starting a for loop 
 for(n in 1:length(aci_file_list))
 {
-print(aci_file_list[n])  
+  print(aci_file_list[n])  
+}
+#need to make loop that will load in all my data
+for(n in 1:length(aci_file_list))
+{
+  
 }
