8502a5d004824c00109491951f677cc160356532
Karew <karenwang@email.arizona.edu>
2015-11-21 21:24:09 -0700

    Script now reads in both the thickness and LMA csv's

--- read_thickness.R
+++ read_thickness.R
@@ -9,3 +9,13 @@ thickness_df=thickness_df[c("Date", "Location", "Node", "Avg.Thickness.mm")]
 thickness_df$Date = strptime(thickness_df$Date, "%m/%d/%y")
 thickness_df$Date = as.Date(thickness_df$Date)
 save(thickness_df, file = "thickness_df.RDA")
+
+#read in LMA from Excel csv
+setwd("C:/Users/Karen/Documents/B2-Honors-Proj/data/")
+LMA_data=read.table("b2 LMA.csv", sep = ",", header = TRUE, stringsAsFactors = FALSE)
+LMA_df= as.data.frame(LMA_data)
+LMA_df=LMA_df[c("Date", "Location", "LMA")]
+LMA_df$Date = strptime(LMA_df$Date, "%m/%d/%Y")
+LMA_df$Date = as.Date(LMA_df$Date)
+save(LMA_df, file = "LMA_df.RDA")
+
